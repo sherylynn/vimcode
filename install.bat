974570856aaed9515f65bae3e30b78f8f6e6e21c
sherylynn <352281674@qq.com>
2018-03-27 19:33:05 +0800

    由于vim在windows下奇怪的特性，所以需要靠windows的批处理直接处理

--- install.bat
+++ install.bat
@@ -41,6 +41,9 @@ call mklink /J "%HOME%\.vim" "%APP_PATH%\"
 IF NOT EXIST "%APP_PATH%\plugged" (
     call mkdir "%APP_PATH%\plugged"
 )
+::下载vim-plug
+call curl -fLo %USERPROFILE%/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 call proxy.bat
 call vim -c "GoInstallBinaries" -c "qa"
