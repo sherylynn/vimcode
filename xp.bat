5d6b796729a82425715a19093c7111e986f447d0
sherylynn <352281674@qq.com>
2018-05-01 00:29:27 +0800

    因为一些默认问题，改回.vim目录

--- xp.bat
+++ xp.bat
@@ -1,6 +1,10 @@
-cd %USERPROFILE%/vimcode
+cd %USERPROFILE%
+git pull http://github.com/sherylynn/vimcode .vim
+cd %USERPROFILE%/.vim
 ::call cat config/_vimrc > ../.vimrc
-echo source %USERPROFILE%\vimcode\config\vimrc > ../.vimrc
+echo let g:VIMHOME=".vim" > ../.vimrc
+::其他设定如果设计后面的，得在source加载前加载
+echo source %USERPROFILE%\.vim\config\vimrc >> ../.vimrc
 mkdir autoload
 cd autoload
 call curl -fLo plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
