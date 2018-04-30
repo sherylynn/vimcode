010a9ceaf7c3cf3d5ecc981340e9d3c2576feb76
sherylynn <352281674@qq.com>
2018-04-30 23:34:02 +0800

    修复了vimrc中使用了相对路径导致的在其他目录获取不到vimcode位置的问题

--- xp.bat
+++ xp.bat
@@ -1,5 +1,6 @@
 cd %USERPROFILE%/vimcode
-call cat config/_vimrc > ../.vimrc
+::call cat config/_vimrc > ../.vimrc
+echo source %USERPROFILE%\vimcode\config\vimrc > ../.vimrc
 mkdir autoload
 cd autoload
 call curl -fLo plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
