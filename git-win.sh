5ccd25ac10f7e12d67c2e315711c588b133c965f
sherylynn <352281674@qq.com>
2018-05-24 23:27:42 +0800

    add new install in win

---
+++ git-win.sh
@@ -0,0 +1,22 @@
+﻿#cd %USERPROFILE%
+#git pull http://github.com/sherylynn/vimcode .vim
+#cd %USERPROFILE%/.vim
+#echo let g:VIMHOME=".vim" > ../.vimrc
+#echo source %USERPROFILE%\.vim\config\vimrc >> ../.vimrc
+
+cd 
+git pull https://github.com/sherylynn/vimcode vimcode
+cd vimcode
+echo let g:VIMHOME="vimcode" > ../.vimrc
+echo source ./vimcode/config/vimrc >> ../.vimrc
+#guifont和后面的内容不能有空格
+echo set guifont=Courier_new:h15:b >> ../.vimrc
+echo set ff=unix >> ../.vimrc
+
+
+#call cat config/_vimrc > ../.vimrc
+#其他设定如果设计后面的，得在source加载前加载
+
+mkdir autoload
+cd autoload
+call curl -fLo plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
