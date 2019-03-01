ced0753bf06c5028c7a099f241fb83381eb877d2
sherylynn <352281674@qq.com>
2019-03-01 21:10:05 +0800

    add git-bash update

--- git-win.sh
+++ git-win.sh
@@ -3,7 +3,7 @@ cd ~
 git pull https://github.com/sherylynn/vimcode vimcode
 cd vimcode
 echo let g:VIMHOME=\"vimcode\" > ../.vimrc
-echo source ~/vimcode/config/vimrc >> ../.vimrc
+echo 'source ~/vimcode/config/vimrc' >> ../.vimrc
 #guifont和后面的内容不能有空格
 echo set guifont=Courier_new:h15:b >> ../.vimrc
 echo set ff=unix >> ../.vimrc
@@ -11,20 +11,13 @@ echo set ff=unix >> ../.vimrc
 #   for neovim in unix
 rm -rf ../.config/nvim
 mkdir -p ../.config/nvim
-echo let g:VIMHOME=\"vimcode\" > ../.config/nvim/init.vim
-echo source ~/vimcode/configinit.vim >> ../.config/nvim/init.vim
-echo set guifont=Courier_new:h15:b >> ../.config/nvim/init.vim
-echo set ff=unix >> ../.config/nvim/init.vim
+cp ../.vimrc ../.config/nvim/init.vim
 
 #   for neovim in windows
 #   或许还是要考虑环境变量$XDG_CONFIG_HOME
 rm -rf ../AppData/Local/nvim
 mkdir -p ../AppData/Local/nvim
-echo let g:VIMHOME=\"vimcode\" > ../AppData/Local/nvim/init.vim
-echo source ~/vimcode/configinit.vim >> ../AppData/Local/nvim/init.vim
-echo set guifont=Courier_new:h15:b >> ../AppData/Local/nvim/init.vim
-echo set ff=unix >> ../AppData/Local/nvim/init.vim
-
+cp ../.vimrc ../AppData/Local/nvim/init.vim
 
 #call cat config/_vimrc > ../.vimrc
 #其他设定如果设计后面的，得在source加载前加载
